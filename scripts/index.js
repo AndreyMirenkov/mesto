ff72b6f390f33299106df17040b976fd758c2fa9
Andrey Mirenkov <Andreymirenkov@yandex.ru>
2021-11-19 12:35:57 +0300

    feat js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,33 @@
-console.log("loaded")
+
+const editButton = document.querySelector(".profile__edit-button_active");
+const popup = document.querySelector(".popup");
+const popupCloseButton = popup.querySelector(".popup__close");
+const formElement = popup.querySelector(".popup__form_edit");
+let nameInput = popup.querySelector(".popup__input_name");
+let workInput = popup.querySelector(".popup__input_work");
+let newName = document.querySelector(".profile__name_edit");
+let newWork = document.querySelector(".profile__text_edit");
+const saveButton = popup.querySelector(".popup__save-button_active");
+
+function open(){
+    nameInput.value = newName.textContent;
+    workInput.value = newWork.textContent;
+    popup.classList.add("popup_opened");
+}
+
+function close(){
+    nameInput.value= "";
+    workInput.value= "";
+    popup.classList.remove("popup_opened");
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    newName.textContent = nameInput.value;
+    newWork.textContent = workInput.value;
+    close();
+}
+
+editButton.addEventListener("click", open);
+popupCloseButton.addEventListener("click", close);
+formElement.addEventListener("submit", formSubmitHandler);
